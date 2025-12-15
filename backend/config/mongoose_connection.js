2e45cfeb78eef6c9a1973b60020f7856decf45e2
Avishkar Kabadi <155943160+Avishkar-Kabadi@users.noreply.github.com>
2025-12-15 12:25:48 +0530

    baseUrl

--- backend/config/mongoose_connection.js
+++ backend/config/mongoose_connection.js
@@ -7,7 +7,7 @@ const dbgr = require("debug")("development:DB");
 
 
 mongoose.connect(`${config.get('MONGODB_URL')}/devtinder`).then(() => {
-    dbgr("DB Connected");
+    dbgr("DB Connected", config.get('MONGODB_URL'));
 }).catch((error) => {
     dbgr(error);
 });
